99f34f27f5d36bd1acfe6e71e325f7efcd8d58bf
yaolongfei <2991205548@qq.com>
2024-10-12 10:09:21 +0800

    fix: 兼容单文件

--- src/util.js
+++ src/util.js
@@ -2,6 +2,10 @@ module.exports = {
     // 遍历对象
     objForEach: function (files, fn) {
         let result;
+        if (!Array.isArray(files)) {
+            result = fn.call(files, 'temp', files);
+            return;
+        }
         for (file of files) {
             result = fn.call(files, 'temp', file);
             if (result === false) {
